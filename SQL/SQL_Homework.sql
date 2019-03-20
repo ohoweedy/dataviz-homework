00b253d960a2d69aa5f899a431a1ba1af6b50672
ohoweedy <howeedy5@gmail.com>
2019-03-20 01:54:25 -0400

    Tues night update

--- SQL/SQL_Homework.sql
+++ SQL/SQL_Homework.sql
@@ -60,11 +60,45 @@ UPDATE actor
 -- 5a
 SHOW CREATE TABLE address;
 
+-- 6a
+SELECT staff.first_name, staff.last_name, address.address
+	FROM staff
+	JOIN address ON
+	staff.address_id = address.address_id;
 
-SELECT * FROM actor;
+-- 6b
+SELECT staff.first_name, staff.last_name, SUM(payment.amount) AS '08/2005 Payments'
+	FROM staff
+	JOIN payment ON
+	staff.staff_id = payment.staff_id AND payment.payment_id >= 22 AND payment_id <= 32
+    GROUP BY staff.staff_id;
 
+-- 6c
+SELECT film.title, COUNT(film_actor.actor_id) AS 'Number of Actors'
+	FROM film_actor
+	INNER JOIN film ON
+	film_actor.film_id = film.film_id
+	GROUP BY film.title;
 
+-- 6d
+SELECT title, 
+	(SELECT COUNT(*) 
+		FROM inventory 
+        WHERE film.film_id = inventory.film_id ) AS 'Film Inventory'
+	FROM film
+	WHERE title = 'HUNCHBACK IMPOSSIBLE';
 
+-- 6e
+SELECT customer.first_name, customer.last_name, SUM(payment.amount) AS 'Total Amount Paid'
+	FROM customer
+	JOIN payment ON
+	customer.customer_id = payment.customer_id
+    GROUP BY customer.customer_id
+    ORDER BY last_name;
+
+-- 7a
+
+    
 
 
 
