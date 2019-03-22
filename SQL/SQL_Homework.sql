3a339a3e66a1e8c5b0067c44596fc702ea51ef17
ohoweedy <howeedy5@gmail.com>
2019-03-22 00:39:52 -0400

    SQL Homework Completed

--- SQL/SQL_Homework.sql
+++ SQL/SQL_Homework.sql
@@ -142,7 +142,7 @@ SELECT f.title, COUNT(r.inventory_id) AS 'Most Frequently Rented Movies'
 	FROM payment AS p
 	JOIN rental AS r ON p.rental_id = r.rental_id
 	JOIN inventory AS i ON r.inventory_id = i.inventory_id
-	JOIN film as f ON f.film_id = i.film_id
+	JOIN film AS f ON f.film_id = i.film_id
 	GROUP BY f.title
     ORDER BY COUNT(*) DESC;
 
